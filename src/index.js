301be9afcb4ae889044b0f9097993a77e3415289
Fred Wang <namelos@outlook.com>
2016-03-07 11:36:52 +0800

    init webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+// import React from 'react'
+// import { render } from 'react-dom'
+// import reducer from './redux'
+// import configureStore from './store'
+// import routes from './routes'
+//
+// const root = document.getElementById('root')
+// const store = configureStore(reducer)
+// const app = routes(store)
+//
+// render(app, root)
 import React from 'react'
 import { render } from 'react-dom'
-import reducer from './redux'
-import configureStore from './store'
-import routes from './routes'
 
-const root = document.getElementById('root')
-const store = configureStore(reducer)
-const app = routes(store)
-
-render(app, root)
+render(<h1>hello test</h1>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const BrowserSync = require('browser-sync-webpack-plugin')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -10,13 +9,6 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new BrowserSync({
-      host: 'localhost',
-      port: 3001,
-      proxy: 'http://localhost:3000'
-    })
-  ],
   module: {
     loaders: [{
       test: /\.jsx?/,
