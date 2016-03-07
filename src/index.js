10d4c0e091a3a1cfbd2d32eabd1ba65b5011c4c1
Fred Wang <namelos@outlook.com>
2016-03-07 12:12:15 +0800

    finish basic redux counter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
 
-class Greetings extends Component {
-  state = { name: 'world' };
-
-  handleChange = e =>
-    this.setState({ name: e.target.value })
-
-  render = () => <div>
-    <input type="text" onChange={ this.handleChange } />
-    <h1>hello, { this.state.name }</h1>
-  </div>
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
 }
 
-render(<Greetings />, document.getElementById('root'))
+const store = createStore(counter)
+
+store.subscribe(() => {
+  render(
+    <div>
+      <button onClick={ () => store.dispatch({ type: 'INCREMENT' })}>INCREMENT</button>
+      <button onClick={ () => store.dispatch({ type: 'DECREMENT' })}>DECREMENT</button>
+      <h1>counter: { store.getState() }</h1>
+    </div>,
+    document.getElementById('root'))
+})
+
+store.dispatch({ type: 'Nothing' })
