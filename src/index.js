b0f11f11eab66ed4b6e592bae50a865b10831247
Fred Wang <namelos@outlook.com>
2016-03-07 11:52:36 +0800

    add nested children

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-const Greetings = ({ name }) => <h1>
-  Hello, { name }!
+const Greetings = ({ children }) => <h1>
+  Hello, { children }!
 </h1>
 
-render(<Greetings name="fred" />, document.getElementById('root'))
+const App = ({ children }) => <div>
+  <h1>Name list:</h1>
+  { children }
+</div>
+
+render(
+  <App>
+    <ul>
+      <li><Greetings>Fred</Greetings></li>
+      <li><Greetings>Tom</Greetings></li>
+      <li><Greetings>Jerry</Greetings></li>
+    </ul>
+  </App>,
+  document.getElementById('root'))
