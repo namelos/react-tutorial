ad4b68150ee7693307c9594a77a1f51de9e09aeb
Fred Wang <namelos@outlook.com>
2016-03-07 11:38:40 +0800

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-// import React from 'react'
-// import { render } from 'react-dom'
-// import reducer from './redux'
-// import configureStore from './store'
-// import routes from './routes'
-//
-// const root = document.getElementById('root')
-// const store = configureStore(reducer)
-// const app = routes(store)
-//
-// render(app, root)
 import React from 'react'
 import { render } from 'react-dom'
 
