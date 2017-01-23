b2089db756db7806ec957d85b37e195b3a2a8608
Yifan Wang <namelos@outlook.com>
2017-01-23 23:07:39 +0800

    install react router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { CounterApp } from './container/CounterApp'
+import { Router, IndexRoute, Route, browserHistory } from 'react-router'
+
 import { counter } from './module/counter'
+
+import { App } from './component/App'
+import { CounterApp } from './container/CounterApp'
 
 const store = createStore(counter)
 
 render(
   <Provider store={store}>
-    <CounterApp />
+    <div>
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={CounterApp}/>
+        </Route>
+      </Router>
+    </div>
   </Provider>, document.querySelector('#app'))
